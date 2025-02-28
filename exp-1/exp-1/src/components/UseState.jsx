4b5336d56f32522e23586603299faea63530ced3
Ariel Pellegrino <ariel.pell@gmail.com>
2025-02-28 16:35:35 -0300

    update: useState

--- exp-1/exp-1/src/components/UseState.jsx
+++ exp-1/exp-1/src/components/UseState.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import {useState} from "react";
 
-export default () => {
+export default function UseState() {
 
-    const [valorOriginal, setNovoValor] = UseState()
-
+    const [contador, setContador] = useState(0)
+    console.log(contador)
     return (
         <>
 
-
+            <p>Altera o estado atual do componente</p>
             <div>
-                <button onChange={}>Muda valor</button>
+                <button onClick={() => setContador(contador + 1)}>Mudar valor
+                </button>
+                {contador}
             </div>
         </>
     )
