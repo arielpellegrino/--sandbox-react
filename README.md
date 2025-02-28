af18aebe17621892749b57ae41832a62981ef26f
Ariel Pellegrino <ariel.pell@gmail.com>
2025-02-28 16:15:47 -0300

    update: exp-1 - useState test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# --sandbox-react
+# Ambiente de sandbox para testar scripts ReactJS
+
+## Stack utilizada
+
+[![My Skills](https://skillicons.dev/icons?i=webstorm,html,css,js,react,redux,webpack,jest,npm,vite,express,nodejs&theme=dark)](https://skillicons.dev)
+
+
---
+++ exp-1/exp-1/src/components/UseState.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default () => {
+
+    const [valorOriginal, setNovoValor] = UseState()
+
+    return (
+        <>
+
+
+            <div>
+                <button onChange={}>Muda valor</button>
+            </div>
+        </>
+    )
+}
